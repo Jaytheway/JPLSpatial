c0d3c9d7cc65509ab85ccb7d2bd36af05374f326
Jaroslav <music.jp.production@gmail.com>
2026-02-16 13:35:18 +0200

    Smoothed Value utility for soothing value changes over time

---
+++ Spatialization/include/JPLSpatial/Utilities/SmoothedValue.h
@@ -0,0 +1,82 @@
+﻿//
+//      ██╗██████╗     ██╗     ██╗██████╗ ███████╗
+//      ██║██╔══██╗    ██║     ██║██╔══██╗██╔════╝		** JPLSpatial **
+//      ██║██████╔╝    ██║     ██║██████╔╝███████╗
+// ██   ██║██╔═══╝     ██║     ██║██╔══██╗╚════██║		https://github.com/Jaytheway/JPLSpatial
+// ╚█████╔╝██║         ███████╗██║██████╔╝███████║
+//  ╚════╝ ╚═╝         ╚══════╝╚═╝╚═════╝ ╚══════╝
+//
+//   Copyright Jaroslav Pevno, JPLSpatial is offered under the terms of the ISC license:
+//
+//   Permission to use, copy, modify, and/or distribute this software for any purpose with or
+//   without fee is hereby granted, provided that the above copyright notice and this permission
+//   notice appear in all copies. THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+//   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+//   AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+//   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+//   WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+//   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#pragma once
+
+#include <cstdint>
+#include <cmath>
+
+namespace JPL
+{
+    template<class T>
+    struct SmoothedValue
+    {
+    public:
+        T Current = T(0.0);
+        T Target = T(0.0);
+        T Alpha = T(1.0);
+
+    public:
+        inline static T GetExpAlpha(uint32_t numOfTicks)
+        {
+            return numOfTicks == 0
+                ? T(1.0)
+                : T(1.0 - std::exp(-1.0 / static_cast<double>(numOfTicks)));
+        }
+
+        inline static T GetInvAlpha(uint32_t numOfTicks)
+        {
+            return numOfTicks == 0
+                ? T(1.0)
+                : T(1.0 / static_cast<double>(numOfTicks));
+        }
+
+        inline static SmoothedValue CreateExpSmoothing(uint32_t numOfTicks)
+        {
+            return SmoothedValue{ .Alpha = GetExpAlpha(numOfTicks) };
+        }
+
+        inline static SmoothedValue CreateSimpleSmoothing(uint32_t numOfTicks)
+        {
+            return SmoothedValue{ .Alpha = GetInvAlpha(numOfTicks) };
+        }
+
+        inline static SmoothedValue CreateExpSmoothing(T current, T target, uint32_t numOfTicks)
+        {
+            return SmoothedValue{ .Current = current, .Target = target, .Alpha = GetExpAlpha(numOfTicks) };
+        }
+
+        inline static SmoothedValue CreateSimpleSmoothing(T current, T target, uint32_t numOfTicks)
+        {
+            return SmoothedValue{ .Current = current, .Target = target, .Alpha = GetInvAlpha(numOfTicks) };
+        }
+
+        inline void SetDurationExp(uint32_t numTicks) { Alpha = GetExpAlpha(numTicks); }
+
+        // Advance smoothing to the next value
+        inline void Tick()
+        {
+            // c = a * c + (1.0f - a) * t : or a sing SUB + FMA
+            Current = Alpha * (Target - Current) + Current;
+        }
+
+        // Advance smoothing to the next value and return it
+        inline T GetNext() { Tick(); return Current; }
+    };
+}
