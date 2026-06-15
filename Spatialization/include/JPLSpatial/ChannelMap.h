73938c87f91b1d0c9f0b33228f688043b0966dd8
Jaroslav <music.jp.production@gmail.com>
2026-06-15 14:36:35 +0300

    Default constructor for `NamedChannelMask`

--- Spatialization/include/JPLSpatial/ChannelMap.h
+++ Spatialization/include/JPLSpatial/ChannelMap.h
@@ -257,6 +257,8 @@ namespace JPL
         {
         }
 
+        NamedChannelMask() noexcept = default;
+
         JPL_INLINE bool operator==(const NamedChannelMask& other) const noexcept { return other.Layout == Layout; }
     };
 } // namespace JPL
